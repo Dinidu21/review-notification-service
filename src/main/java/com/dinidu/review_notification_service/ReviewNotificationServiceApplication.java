958c7f8a96f842e930bdc9493bd9fa4b0f0248fd
Dinidu21 <dinidusachintha3@gmail.com>
2026-08-09 09:39:15 +0530

    Add initial project structure with Maven configuration and Spring Boot application

---
+++ src/main/java/com/dinidu/review_notification_service/ReviewNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dinidu.review_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinidu/review_notification_service/ReviewNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinidu.review_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

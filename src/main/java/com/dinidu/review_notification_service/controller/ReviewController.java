11a2b880e52bd2c463e2680115536d5387877a11
Dinidu21 <dinidusachintha3@gmail.com>
2026-08-09 16:27:01 +0530

    refactor: remove unnecessary RequestMapping annotation from ReviewController

--- src/main/java/com/dinidu/review_notification_service/controller/ReviewController.java
+++ src/main/java/com/dinidu/review_notification_service/controller/ReviewController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/reviews")
 public class ReviewController {
 
     private final ReviewRepository reviewRepository;
